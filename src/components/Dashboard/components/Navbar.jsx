c72ac85d97b7aa4f2d9f1db765e63dedc3c14bed
unknown <pranavbharadwaj002@gmail.com>
2023-06-07 22:47:35 +0530

    AiOutlineMenu error solved

--- src/components/Dashboard/components/Navbar.jsx
+++ src/components/Dashboard/components/Navbar.jsx
@@ -1,95 +1,76 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { BsChatLeft } from 'react-icons/bs';
 import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
-import { useAppState } from '../../../store/index.js';
 
-const Navbar = () => {
-    const NavButton = ({ title, customFunction, icon, color, dotColor }) => (
- 
-        <button type="button" onClick={customFunction} style={{ color }} className="relative p-3 text-xl rounded-full hover:bg-light-gray">
-          <span 
-            style={{ background: dotColor }}
-            className="absolute inline-flex w-2 h-2 rounded-full right-2 top-2"
-          />
-          {icon}
-        </button>
-      
-    );
+const Navbar = ({ currentColor, screenSize ,setActiveMenu}) => {
+  const NavButton = ({ title, customFunction, icon, color, dotColor }) => (
+    <button type="button" onClick={customFunction} style={{ color }} className="relative p-3 text-xl rounded-full hover:bg-light-gray">
+      <span
+        style={{ background: dotColor }}
+        className="absolute inline-flex w-2 h-2 rounded-full right-2 top-2"
+      />
+      {icon}
+    </button>
+  );
 
-    const setActiveMenu = useAppState((state) => state.setActiveMenu);
-    const isClicked = useAppState((state) => state.isClicked);
-    const setIsClicked = useAppState((state) => state.setIsClicked);
-    const setScreenSize = useAppState((state) => state.setScreenSize);
-    const currentColor = useAppState((state) => state.currentColor);
-    const screenSize = useAppState((state) => state.screenSize);
-    const activeMenu = useAppState((state) => state.activeMenu);
-    
-    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true});
+  const [isClicked, setIsClicked] = useState({});
   
+  const handleClick = (clicked) => setIsClicked({ ...isClicked, [clicked]: true });
+
   useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth);
+    const handleResize = () => {
+      const windowWidth = window.innerWidth;
+      if (windowWidth <= 900) {
+        setActiveMenu(false);
+      } else {
+        setActiveMenu(true);
+      }
+    };
 
     window.addEventListener('resize', handleResize);
     handleResize();
 
-    return window.removeEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
-
-  useEffect(() => {
-    if (screenSize <= 900) {
-      setActiveMenu(false);
-    } else {
-      setActiveMenu(true);
-    }
-  }, [screenSize]);
 
   return (
     <div className="relative flex justify-between p-2 md:mx-6">
-      <NavButton 
-        title="Menu" 
-        customFunction={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
-        color={currentColor} 
-        icon={<AiOutlineMenu />} 
+      <NavButton
+        title="Menu"
+        customFunction={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color={currentColor}
+        icon={<AiOutlineMenu />}
       />
-
       <div className="flex">
-        <NavButton 
-          title="Chat" 
-          customFunction={() => handleClick('chat')} 
-          color={currentColor} 
+        <NavButton
+          title="Chat"
+          customFunction={() => handleClick('chat')}
+          color={currentColor}
           dotColor="#03C9D7"
-          icon={<BsChatLeft />} 
+          icon={<BsChatLeft />}
         />
-        <NavButton 
-          title="Notifications" 
-          customFunction={() => handleClick('notification')} 
-          color={currentColor} 
+        <NavButton
+          title="Notifications"
+          customFunction={() => handleClick('notification')}
+          color={currentColor}
           dotColor="#03C9D7"
-          icon={<RiNotification3Line />} 
+          icon={<RiNotification3Line />}
         />
-       
-          <div 
-            className="flex items-center gap-2 p-1 rounded-lg cursor-pointer hover:bg-light-gray"
-            onClick={() => handleClick('userProfile')}
-          >
-            {/* <img className="w-8 h-8 rounded-full" src={profile} alt="r" /> */}
-            <p>
-              <span className="text-gray-400 text-14">Hi, </span> {' '}
-              <span className="ml-1 font-bold text-gray-400 text-14">Pranav</span>
-            </p>
-            <MdKeyboardArrowDown className="text-gray-400 text-14" />
-          </div>
-       
-
-        {/* {isClicked.cart && <Cart />}
-        {isClicked.chat && <Chat />}
-        {isClicked.notification && <Notification />}
-        {isClicked.userProfile && <UserProfile />} */}
+        <div
+          className="flex items-center gap-2 p-1 rounded-lg cursor-pointer hover:bg-light-gray"
+          onClick={() => handleClick('userProfile')}
+        >
+          <p>
+            <span className="text-gray-400 text-14">Hi, </span>
+            <span className="ml-1 font-bold text-gray-400 text-14">Pranav</span>
+          </p>
+          <MdKeyboardArrowDown className="text-gray-400 text-14" />
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- src/store/index.js
+++ src/store/index.js
@@ -13,33 +13,33 @@ const useBearStore = create((set) => ({
   }));
 
   
-const useAppState = create((set) => ({
-  activeMenu: true,
-  isClicked: {
-    chat: false,
-    cart: false,
-    userProfile: false,
-    notification: false,
-  },
-  screenSize: undefined,
-  currentColor: localStorage.getItem('colorMode') || '#03C9D7',
-  currentMode: localStorage.getItem('themeMode') || 'Light',
-  themeSettings: false,
-  setActiveMenu: (activeMenu) => set({ activeMenu }),
-  setIsClicked: (isClicked) => set({ isClicked }),
-  setScreenSize: (screenSize) => set({ screenSize }),
-  setColor: (color) => {
-    localStorage.setItem('colorMode', color);
-    set({ currentColor: color, themeSettings: false });
-  },
-  setMode: (mode) => {
-    localStorage.setItem('themeMode', mode);
-    set({ currentMode: mode, themeSettings: false });
-  },
-  setThemeSettings: (themeSettings) => set({ themeSettings }),
-}));
+// const useAppState = create((set) => ({
+//   activeMenu: true,
+//   isClicked: {
+//     chat: false,
+//     cart: false,
+//     userProfile: false,
+//     notification: false,
+//   },
+//   screenSize: undefined,
+//   currentColor: localStorage.getItem('colorMode') || '#03C9D7',
+//   currentMode: localStorage.getItem('themeMode') || 'Light',
+//   themeSettings: false,
+//   setActiveMenu: (activeMenu) => set({ activeMenu }),
+//   setIsClicked: (isClicked) => set({ isClicked }),
+//   setScreenSize: (screenSize) => set({ screenSize }),
+//   setColor: (color) => {
+//     localStorage.setItem('colorMode', color);
+//     set({ currentColor: color, themeSettings: false });
+//   },
+//   setMode: (mode) => {
+//     localStorage.setItem('themeMode', mode);
+//     set({ currentMode: mode, themeSettings: false });
+//   },
+//   setThemeSettings: (themeSettings) => set({ themeSettings }),
+// }));
 
-  export {useBearStore,useStore,useAppState} ;
+  export {useBearStore,useStore} ;
 
 
   
